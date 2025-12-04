a2dac7d8a1dba03b98c09a7631934f8c1cd46c26
jonahhess <jonahhessdev@gmail.com>
2025-12-05 00:41:40 +0200

    added db

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
+	"myapp/db"
 	"myapp/handlers"
+	"net/http"
 )
 
 func main() {
+    // Initialize SQLite database
+    err := db.InitDB("myapp.db")
+    if err != nil {
+        panic(fmt.Sprintf("Database init failed: %v", err))
+    }
+    db.CreateTables()
+
     http.HandleFunc("/", handlers.RootHandler)
     http.HandleFunc("/load-partial/", handlers.PartialHandler)
     http.HandleFunc("/dashboard", handlers.DashboardHandler)
