a63ec0cdc541e4bf487065898abcd168020091c7
jonahhess <jonahhessdev@gmail.com>
2026-01-08 12:11:11 +0200

    serve static, 404 bad paths

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ import (
 )
 
 func main() {
-    // Initialize SQLite database
     err := db.InitDB("db/myapp.db")
     if err != nil {
         panic(fmt.Sprintf("Database init failed: %v", err))
     }
     db.CreateTables()
 
-    http.HandleFunc("/", handlers.RootHandler)
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+            http.NotFound(w, r)
+            return
+        }
+        handlers.RootHandler(w, r)
+    })
     http.HandleFunc("/load-partial/", handlers.PartialHandler)
     http.HandleFunc("/dashboard", handlers.DashboardHandler)
+
+    fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 
     fmt.Println("Server running on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
