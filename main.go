a8d2ac333999cbfb6aae4b42c7c33a2476082808
jonahhess <jonahhessdev@gmail.com>
2026-01-08 14:06:01 +0200

    router in separate file

--- main.go
+++ main.go
@@ -3,28 +3,19 @@ package main
 import (
 	"fmt"
 	"myapp/db"
-	"myapp/handlers"
+	"myapp/router"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-    err := db.InitDB("db/myapp.db")
-    if err != nil {
-        panic(fmt.Sprintf("Database init failed: %v", err))
-    }
-    db.CreateTables()
+	err := db.InitDB("db/myapp.db")
+	if err != nil {
+		panic(fmt.Sprintf("Database init failed: %v", err))
+	}
+	db.CreateTables()
 
-    r := chi.NewRouter()
+	r := router.SetupRoutes()
 
-    r.Get("/", handlers.RootHandler)
-    r.Get("/dashboard", handlers.DashboardHandler)
-    r.Get("/reports", handlers.ReportsHandler)
-
-    fs := http.FileServer(http.Dir("./static"))
-    r.Handle("/static/*", http.StripPrefix("/static/", fs))
-
-    fmt.Println("Server running on http://localhost:8080")
-    http.ListenAndServe(":8080", r)
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
