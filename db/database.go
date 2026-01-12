1381405cb76d73e17faadaddcd52de07e57bce6a
Jonah Hess <61464306+Jonahhess@users.noreply.github.com>
2026-01-12 23:20:31 +0200

    match user struct

--- db/database.go
+++ db/database.go
@@ -33,6 +33,7 @@ func CreateTables() error {
 	query := `
 	CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
