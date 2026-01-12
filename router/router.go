0b69f8a276986a013d8819b564a926878b8be54e
Jonah Hess <61464306+Jonahhess@users.noreply.github.com>
2026-01-12 16:36:19 +0200

    used gorilla middlewares

--- router/router.go
+++ router/router.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	home "myapp/pages/home"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/sessions"
 )
 
@@ -15,6 +17,13 @@ func SetupRoutes(
 ) *chi.Mux {
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Handle("/static/*",
 		http.StripPrefix("/static/",
