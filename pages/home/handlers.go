87750293976b180805f4ee4d19d8e47ad0a47a70
Jonah Hess <61464306+Jonahhess@users.noreply.github.com>
2026-01-12 20:41:19 +0200

    removed prop drill from page

--- pages/home/handlers.go
+++ pages/home/handlers.go
@@ -2,14 +2,13 @@ package home
 
 import (
 	"myapp/layouts"
-	"myapp/types"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := layouts.
-		Base("Home", Home(types.User{})).
+		Base("Home", Home()).
 		Render(r.Context(), w)
 
 	if err != nil {
