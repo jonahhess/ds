78eb867237c340b97b74776cbf973011d76217f6
Jonah Hess <61464306+Jonahhess@users.noreply.github.com>
2026-01-14 23:26:02 +0200

    added back chi middlewares

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"net/http"
+	"time"
 
 	"myapp/components/navbar"
 	middlewares "myapp/middlewares"
@@ -12,18 +13,19 @@ import (
 	"myapp/pages/signup"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/sessions"
 )
 
 func SetupRoutes(sessionStore *sessions.CookieStore) *chi.Mux {
 	r := chi.NewRouter()
 
-	//r.Use(middleware.RequestID)
-	//r.Use(middleware.RealIP)
-	//r.Use(middleware.Logger)
-	//r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	//r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Use(middlewares.SessionMiddleware(sessionStore))
 	r.Use(middlewares.FlashMiddleware)
