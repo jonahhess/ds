3a1bce2d9a4bcbd0ac01fd8df1e12c7a2d58e7bf
jonahhess <jonahhessdev@gmail.com>
2026-01-19 00:26:15 +0200

    improved queries

--- db/database.go
+++ db/database.go
@@ -16,8 +16,11 @@ func InitDB(path string) error {
 		return err
 	}
 
-	// Optional: set pragmas for performance
-	DB.Exec("PRAGMA foreign_keys = ON;")
+	DB.Exec(`
+        PRAGMA foreign_keys = ON;
+        PRAGMA journal_mode = WAL;
+        PRAGMA synchronous = NORMAL;
+    `)
 
 	return DB.Ping()
 }
@@ -31,7 +34,7 @@ func CloseDB() error {
 
 func CreateTables() error {
 	users := `
-	CREATE TABLE users (
+	CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
@@ -43,77 +46,90 @@ func CreateTables() error {
 	DB.Exec(users)
 
 	courses := `
-	CREATE TABLE courses (
+	CREATE TABLE IF NOT EXISTS courses (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT NOT NULL,
   description TEXT,
-  created_by TEXT NOT NULL,
+  created_by INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  FOREIGN KEY (created_by) REFERENCES users(id)
+
 );
     `
  DB.Exec(courses)
 
  	lessons := `
-	CREATE TABLE lessons (
+	CREATE TABLE IF NOT EXISTS lessons (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  course_id INTEGER FOREIGN KEY,
-  quiz_id INTEGER FOREIGN KEY, 
+  course_id INTEGER NOT NULL,
   title TEXT NOT NULL,
   text TEXT NOT NULL,
-  created_by TEXT NOT NULL,
+  created_by INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+  FOREIGN KEY (created_by) REFERENCES users(id)
 );
     `
  DB.Exec(lessons);
 
   quizzes := `
-	CREATE TABLE quizzes (
+	CREATE TABLE IF NOT EXISTS quizzes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  lesson_id INTEGER FOREIGN KEY UNIQUE
+  lesson_id INTEGER NOT NULL UNIQUE,
+  FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON DELETE CASCADE
 );
     `
  DB.Exec(quizzes);
 
   questions := `
-	CREATE TABLE questions (
+	CREATE TABLE IF NOT EXISTS questions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  quiz_id INTEGER FOREIGN KEY,
+  quiz_id INTEGER NOT NULL,
   text TEXT NOT NULL,
-  created_by TEXT NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  created_by INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE,
+  FOREIGN KEY (created_by) REFERENCES users(id)
 );
     `
  DB.Exec(questions);
 
   answers := `
-	CREATE TABLE answers (
+	CREATE TABLE IF NOT EXISTS answers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  question_id INTEGER FOREIGN KEY,
-  text TEXT NOT NULL
+  question_id INTEGER NOT NULL,
+  text TEXT NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
+
 );
     `
  DB.Exec(answers);
 
-  correctAnswer := `
-	CREATE TABLE correctAnswer (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  question_id INTEGER FOREIGN KEY,
-  answer_id INTEGER FOREIGN KEY
+  correct_answers := `
+	CREATE TABLE IF NOT EXISTS correct_answers (
+  question_id INTEGER NOT NULL,
+  answer_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
+  FOREIGN KEY (answer_id) REFERENCES answers(id)
 );
     `
- DB.Exec(correctAnswer);
+ DB.Exec(correct_answers);
 
   reviewcards := `
-	CREATE TABLE reviewcards (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER FOREIGN KEY,
-  question_id INTEGER FOREIGN KEY,
-  consecutiveSuccesses INTEGER DEFAULT 0,
-  successes INTEGER DEFAULT 0,
-  reviews INTEGER DEFAULT 0,
-  created_by TEXT NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  CREATE TABLE IF NOT EXISTS reviewcards (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  review_at DATETIME (datetime('now', '+1 day')),
+  consecutive_successes INTEGER DEFAULT 0 CHECK (consecutive_successes >= 0),
+  successes INTEGER DEFAULT 0 CHECK (successes >= 0),
+  reviews INTEGER DEFAULT 0 CHECK (reviews >= 0),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (user_id, question_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 );
+
     `
  DB.Exec(reviewcards);
 
