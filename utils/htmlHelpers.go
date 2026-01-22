da85628da42989fb90d250e650633dd2286abdfe
jonahhess <jonahhessdev@gmail.com>
2026-01-22 13:23:28 +0200

    use better routing

--- utils/htmlHelpers.go
+++ utils/htmlHelpers.go
@@ -3,9 +3,13 @@ package utils
 import (
 	"context"
 	"fmt"
+	"myapp/auth"
 )
 
 func UserLink(ctx context.Context, path string) string {
-	userID := GetUserID(ctx)
+	userID, ok := auth.UserIDFromContext(ctx)
+	if !ok {
+		return "/notfound"
+	}
 	return fmt.Sprintf("/%d/%s", userID, path)
 }
