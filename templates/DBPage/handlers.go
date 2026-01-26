07867a14ac63030a1bc46a1fc079368c0f1d88b2
jonahhess <jonahhessdev@gmail.com>
2026-01-26 10:54:16 +0200

    updated db template

--- templates/DBPage/handlers.go
+++ templates/DBPage/handlers.go
@@ -1,32 +1,52 @@
 package template
 
 import (
-	"context"
 	"database/sql"
-	"fmt"
+	"myapp/auth"
 	"myapp/layouts"
+	"myapp/types"
 	"net/http"
 )
 
 func Page(DB *sql.DB) http.HandlerFunc {
  return func(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	userID, ok := auth.UserIDFromContext(ctx)
+	if !ok {
+		return
+	}
 
-	 err := layouts.
-	 Base("Template", Template(DB)).
-	 Render(r.Context(), w)
-	 
-	 if err != nil {
+	myData, err := GetAllTemplate(DB, userID)
+	if err != nil {
+		 http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	 if err := layouts.
+	 Base("MyTemplate", MyTemplate(userID, myData)).
+	 Render(r.Context(), w);  err != nil {
 		 http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
 }
 
-func GetAllTemplate(ctx context.Context, DB *sql.DB) sql.Result{
-	query := "select title from template"
-	result, err := DB.Exec(query)
+func GetAllTemplate(DB *sql.DB, userID int) ([]types.Item, error){
+
+	rows, err := DB.Query("SELECT * FROM template")
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	fmt.Println(result)
-	return result
+	defer rows.Close()
+
+	var items []types.Item
+	for rows.Next() {
+		var item types.Item
+		if err := rows.Scan(&item.ID, &item.Text); err != nil {
+			return items, err
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return items, err
+	}
+	
+	return items, nil
 }
