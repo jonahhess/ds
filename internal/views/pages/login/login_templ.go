2269704db4872a7a7f1b7b979d1c5ab176b129cb
jonahhess <jonahhessdev@gmail.com>
2026-01-26 12:01:15 +0200

    generated templ components

--- internal/views/pages/login/login_templ.go
+++ internal/views/pages/login/login_templ.go
@@ -5,9 +5,10 @@ package login
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
+
 import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
 	"github.com/jonahhess/ds/internal/auth"
 	"github.com/jonahhess/ds/internal/views/components/toast"
 )
