89fe53f12d954dc9e1eefb85d6bd088338415921
jonahhess <jonahhessdev@gmail.com>
2026-02-16 23:08:38 +0200

    added many fields to review card

--- internal/types/reviewCard.go
+++ internal/types/reviewCard.go
@@ -1,8 +1,26 @@
 package types
 
+import "time"
+
 type ReviewCard struct {
-    UserID     int
-    QuestionID int
-    Question   string
-    Answers    []Answer
+	ID           int
+	UserID       int
+	QuestionID   int
+	QuestionText string
+	Answers      []Answer
+	ReviewAt     time.Time
+	Interval     int
+	Easiness     float64
+	Repetitions  int
+	Successes    int
+	Reviews      int
+	CreatedAt    time.Time
+}
+
+type ReviewStats struct {
+	TotalCards     int
+	CardsDueToday  int
+	CardsDueSoon   int
+	TotalReviews   int
+	SuccessRate    float64
 }
