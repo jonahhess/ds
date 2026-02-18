2f0ca3546f54f62f239960b1185f91743f041fff
jonahhess <jonahhessdev@gmail.com>
2026-02-18 10:17:06 +0200

    removed unused fn

--- internal/views/layouts/base_templ.go
+++ internal/views/layouts/base_templ.go
@@ -9,17 +9,9 @@ import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
 import (
-	"context"
 	"github.com/jonahhess/ds/internal/auth"
 	"github.com/jonahhess/ds/internal/views/components/navbar"
 )
-
-func GetUser(ctx context.Context) string {
-	if user, ok := ctx.Value("user").(string); ok {
-		return user
-	}
-	return ""
-}
 
 func Base(page string, content templ.Component) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
@@ -49,7 +41,7 @@ func Base(page string, content templ.Component) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(page)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/layouts/base.templ`, Line: 20, Col: 16}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/layouts/base.templ`, Line: 12, Col: 16}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
